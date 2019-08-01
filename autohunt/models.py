9d843c77e3d6a11a786b9920ef20c8d3e1ea2551
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-08-01 13:26:16 -0400

    Created Player model and ran migrations.

--- autohunt/models.py
+++ autohunt/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
 
+class Player(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    api_key = models.IntegerField()
+    name = models.CharField(max_length=255)
+    cooldown = models.IntegerField(null=True)
+    encumbrance = models.IntegerField(null=True)
+    strength = models.IntegerField(null=True)
+    speed = models.IntegerField(null=True)
+    gold = models.IntegerField(null=True)
+    inventory = models.TextField(blank=True)
+    status = models.TextField(blank=True)
+    errors = models.TextField(blank=True)
+    messages = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'#{self.username} {self.name}'
+
+
 class Room(models.Model):
     room_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
